91ee45186b2b3c16806c4894ff1b4e2614521952
Ana <anathayzaribeirodesousa@gmail.com>
2026-06-26 10:45:52 -0300

    fix: vite

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -20,6 +20,8 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 5173,
+      strictPort: true,
+      allowedHosts: true,
       proxy: {
         "/api": {
           target: apiTarget,
